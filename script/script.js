56faa83d445fbb00e9a9fefa65908d1042b2f7e3
HangerThem <f.borisjuk@gmail.com>
2022-04-04 20:29:34 +0200

    Update security

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 //Fetch data from weather API
 function fetchWeatherData(city) {
-    const apiKey = '8aed05046bf86a12a697c1f48668e950';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     fetch(url)
         .then(response => response.json())
